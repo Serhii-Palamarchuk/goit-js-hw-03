92765c913339e19e6c633244c1768b28b183b094
Serhii-Palamarchuk <serhiy.palamarchuk@gmail.com>
2025-01-16 14:42:04 +0200

    Implement slugify function to convert titles into slugs

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,22 @@
 
+/*Задача 1. Генератор slug
+Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і повертає slug, 
+створений із цього рядка.
+ - Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
+ - Усі символи slug повинні бути в нижньому регістрі.
+ - Усі слова slug повинні бути розділені тире.
 
+*/
 'use strict';
 
 console.log("<=== TASK-1 ============================================>");
 
-console.log('Hello, world!');
+function slugify(title) {
+  return title.toLowerCase().split(" ").join("-");
+}
+
+// Код для перевірки
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
